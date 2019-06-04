d6d702a5cfdf3841cb3322f33772fcc752b5e8ac
Nick Morley <nick.morley111@gmail.com>
2019-06-04 13:06:37 +0200

    Add dump_record_examples script

---
+++ scripts/dump_record_examples.py
@@ -0,0 +1,30 @@
+import os
+import requests
+import json
+from config import config
+
+tables = [
+    'documents',
+    'sentences',
+    'tokens',
+    'patterns',
+]
+
+url = config['db_rest_url']
+
+target_dir = 'databases/record_examples'
+
+for table in tables:
+    query = '{}/?id=1'.format(table)
+    query_url = '{0}{1}'.format(url, query)
+    response = requests.get(query_url)
+    try:
+        data = response.json()
+        json_string = json.dumps(data, ensure_ascii=False, indent=2)
+        file_name = '{}.json'.format(table)
+        file_path = os.path.join(target_dir, file_name)
+        with open(file_path, 'w') as f:
+            f.write(json_string)
+        print('Wrote to {}'.format(file_path))
+    except Exception as e:
+        print('Error parsing: \"{}\"'.format(response.text))
---
+++ scripts/dump_record_examples.sh
@@ -0,0 +1,2 @@
+source env/bin/activate
+PYTHONPATH="." python scripts/dump_record_examples.py
